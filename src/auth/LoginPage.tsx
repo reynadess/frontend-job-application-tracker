4d8a2f7d7dc5eab94d9edd4c63821a72dcc815bd
Om Bhor <ombhor617@gmail.com>
2025-02-07 13:46:03 +0530

    Login page schema

--- src/auth/LoginPage.tsx
+++ src/auth/LoginPage.tsx
@@ -3,26 +3,23 @@ import { Input } from "@/components/ui/input";
 import { Separator } from "@/components/ui/separator";
 import { useUserStore } from "@/hooks/zustand/store/useUserStore";
 import { LoginInputType, userLoginSchema } from "@/schema/userSchema";
-import { Loader2, LockKeyhole, Mail, Notebook, User, User2 } from "lucide-react";
+import { Loader2, LockKeyhole, User } from "lucide-react";
 import { ChangeEvent, FormEvent, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 const LoginPage = () => {
   const [input, setInput] = useState<LoginInputType>({
-    firstname:"",
-    lastname:"",
-    username:"",
-    email: "",
+    username: "",
     password: "",
   });
-  const {login} = useUserStore();
+  const { login } = useUserStore();
   const navigate = useNavigate();
   const [errors, setErrors] = useState<Partial<LoginInputType>>({});
   const changeEventHandler = (e: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
     setInput({ ...input, [name]: value });
   };
-  const submitHandler =async (e: FormEvent) => {
+  const submitHandler = async (e: FormEvent) => {
     e.preventDefault();
     console.log(input); //to check whether input is coming or not
     const result = userLoginSchema.safeParse(input);
@@ -34,118 +31,80 @@ const LoginPage = () => {
     try {
       await login(input);
       navigate("/dashboard/job-tracker");
-    } catch (error:any) {
+    } catch (error: any) {
       console.log(error);
     }
   };
   const loading = false;
   return (
     <div className="flex items-center  justify-center min-h-screen ">
-    <form onSubmit={submitHandler} className="md:p-8 w-full max-w-md md:border border-gray-200 rounde  mx-4">
-      <div className="mb-4">
-        <h1 className="font-bold text-center text-2xl">
-          Welcome Back
-        </h1>
-      </div>
-      <div className="flex items-center gap-2">
+      <form
+        onSubmit={submitHandler}
+        className="md:p-8 w-full max-w-md md:border border-gray-200 rounde  mx-4"
+      >
+        <div className="mb-4">
+          <h1 className="font-bold text-center text-2xl">Welcome Back</h1>
+        </div>
+
         <div className="mb-4">
           <div className="relative">
             <Input
               onChange={changeEventHandler}
-              value={input.firstname}
+              value={input.username}
               type="text"
-              name="firstname"
-              placeholder="John"
+              name="username"
+              placeholder="John123"
               className="pl-10 focus-visible:ring-1"
             />
-            <User2 className="absolute inset-y-2 left-2 text-gray-500 pointer-events-none" />
-            {errors && <span className="text-red-500 text-xs">{errors.firstname}</span>}
+            <User className="absolute inset-y-2 left-2 text-gray-500 pointer-events-none" />
+            {errors && (
+              <span className="text-red-500 text-xs">{errors.username}</span>
+            )}
           </div>
         </div>
         <div className="mb-4">
           <div className="relative">
             <Input
               onChange={changeEventHandler}
-              value={input.lastname}
-              type="text"
-              name="lastname"
-              placeholder="Doe"
+              value={input.password}
+              type="password"
+              name="password"
+              placeholder="******"
               className="pl-10 focus-visible:ring-1"
             />
-            <Notebook className="absolute inset-y-2 left-2 text-gray-500 pointer-events-none" />
-            {errors && <span className="text-red-500 text-xs">{errors.lastname}</span>}
+            <LockKeyhole className="absolute inset-y-2 left-2 text-gray-500 pointer-events-none" />
+            {errors && (
+              <span className="text-red-500 text-xs">{errors.password}</span>
+            )}
           </div>
         </div>
-      </div>
-      <div className="mb-4">
-        <div className="relative">
-          <Input
-            onChange={changeEventHandler}
-            value={input.username}
-            type="text"
-            name="username"
-            placeholder="John123"
-            className="pl-10 focus-visible:ring-1"
-          />
-          <User className="absolute inset-y-2 left-2 text-gray-500 pointer-events-none" />
-          {errors && <span className="text-red-500 text-xs">{errors.username}</span>}
-        </div>
-      </div>
-      <div className="mb-4">
-        <div className="relative">
-          <Input
-            onChange={changeEventHandler}
-            value={input.email}
-            type="email"
-            name="email"
-            placeholder="john@example.com"
-            className="pl-10 focus-visible:ring-1"
-          />
-          <Mail className="absolute inset-y-2 left-2 text-gray-500 pointer-events-none" />
-          {errors && <span className="text-red-500 text-xs">{errors.email}</span>}
-        </div>
-      </div>
-      <div className="mb-4">
-        <div className="relative">
-          <Input
-            onChange={changeEventHandler}
-            value={input.password}
-            type="password"
-            name="password"
-            placeholder="******"
-            className="pl-10 focus-visible:ring-1"
-          />
-          <LockKeyhole className="absolute inset-y-2 left-2 text-gray-500 pointer-events-none" />
-          {errors && <span className="text-red-500 text-xs">{errors.password}</span>}
-        </div>
-      </div>
-      {loading ? (
-        <Button
-          disabled
-          className="bg-green hover:bg-hoverGreen w-full border-none"
-        >
-          <Loader2 className="animate-spin h-4 w-4 mr-2" />
-          Please Wait
-        </Button>
-      ) : (
-        <Button
-          type="submit"
-          className="bg-green hover:bg-hoverGreen w-full border-none"
-        >
-          Signup
-        </Button>
-      )}
+        {loading ? (
+          <Button
+            disabled
+            className="bg-green hover:bg-hoverGreen w-full border-none"
+          >
+            <Loader2 className="animate-spin h-4 w-4 mr-2" />
+            Please Wait
+          </Button>
+        ) : (
+          <Button
+            type="submit"
+            className="bg-green hover:bg-hoverGreen w-full border-none"
+          >
+            Login
+          </Button>
+        )}
 
-      <Separator className="mt-7" />
+        <Separator className="mt-7" />
 
-      <p className="mt-4">
-        Don't have an account?{" "}
-        <Link to="/signup" className="text-blue-500 hover:underline">
-          Create new Account
-        </Link>
-      </p>
-    </form>
-  </div>
+        <p className="mt-4">
+          Don't have an account?{" "}
+          <Link to="/signup" className="text-blue-500 hover:underline">
+            Create new Account
+          </Link>
+        </p>
+      </form>
+    </div>
   );
 };
 
